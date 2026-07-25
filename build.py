6f7d1fbd5468a4f8fb65c2b5644b66936b838f0e
Liberty Bell Jerseys <111010901+libertybelljerseys@users.noreply.github.com>
2026-07-25 11:13:31 -0400

    style

--- build.py
+++ build.py
@@ -119,6 +119,8 @@ HEAD_TEMPLATE = """\
 <head>
 <meta charset="utf-8">
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
+<link rel="icon" type="image/png" href="images/favicon.png">
+<link rel="apple-touch-icon" href="images/favicon.png">
 <link rel="preconnect" href="https://fonts.googleapis.com">
 <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
 <link href="https://fonts.googleapis.com/css2?family=Barlow+Condensed:wght@400;600;700;900&family=Barlow:wght@400;500;600&display=swap" rel="stylesheet">
